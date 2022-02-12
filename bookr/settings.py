995a37ce18b41655267643085f0efe5834eb09d8
Chandr <cksahuu@gmail.com>
2022-02-12 23:49:00 +0530

    set Debug : False     lets deploye

--- bookr/settings.py
+++ bookr/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [(os.getenv('ALLOWED_HOSTS'))]
 
