176531a9ce01c88b02f39d391139ef0cf0121c3f
Chandr <cksahuu@gmail.com>
2022-02-13 23:51:55 +0530

    postgres db

--- bookr/settings.py
+++ bookr/settings.py
@@ -79,8 +79,16 @@ WSGI_APPLICATION = 'bookr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookr_store', 
+        'USER': 'postgres', 
+        'PASSWORD': 'postgres', 
+        'HOST': 'localhost',
+        'PORT': '5432' 
+        
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
