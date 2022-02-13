4668c2787904d5496c3ae042cceda03e98b79f73
Chandr <cksahuu@gmail.com>
2022-02-13 13:02:50 +0530

    added allowed host

--- bookr/settings.py
+++ bookr/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-z#&#c=8tz=mi1ko6llt2+tlxfn(k-^cez&9fhd1k8=__mck#pz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://bookr-store.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
